31907dce01f6447986973c9921596d4af339eeaf
Absaar-Ali <absaarali2256@gmail.com>
2021-12-27 19:34:19 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+
+let db;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/employee", async (req, res) => {
+  const employee = [];
+  await db
+    .collection("employee")
+    .find()
+    .forEach((element) => {
+      employee.push(element);
+    });
+  console.log(employee);
+  res.send(employee);
+});
+
+app.post("/api/uptemployee", (req, res) => {
+  const obj_id = req.body.obj_id;
+  const obj = req.body.obj;
+  db.collection("employee")
+    .updateOne(
+      { _id: ObjectId(obj_id.id) },
+      {
+        $set: obj,
+      }
+    )
+    .then(
+      db
+        .collection("employee")
+        .findOne({ _id: ObjectId("61c1daaaabefd6762c7ebd4f") })
+        .then(function (document) {
+          res.send(document);
+        })
+    );
+});
+
+app.post("/api/newemployee", (req, res) => {
+  const obj = req.body;
+  db.collection("employee")
+    .findOne(obj)
+    .then(function (document) {
+      if (document !== null) {
+        res.json("Object Already Present");
+      } else {
+        db.collection("employee")
+          .insertOne(obj)
+          .then(function () {
+            res.json(obj);
+          });
+      }
+    });
+});
+
+const PORT = process.env.PORT || 7000;
+
+const client = new MongoClient(
+  "mongodb+srv://Absaar:password1548@cluster0.oaxoi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+
+client.connect().then(function (mClient) {
+  db = mClient.db();
+  console.log("Mongodb Connection is successful");
+  app.listen(PORT, function () {
+    console.log("Server Started on port 7000");
+  });
+});
